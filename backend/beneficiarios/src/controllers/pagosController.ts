fa3185b4e78c320ecfd32ce4f0255cb8dbb91376
JhairRoussell2 <jhairrmb3@gmail.com>
2025-07-22 02:13:29 -0500

    Cambio de claveapi por una variable para seguridad

--- backend/beneficiarios/src/controllers/pagosController.ts
+++ backend/beneficiarios/src/controllers/pagosController.ts
@@ -1,28 +1,28 @@
-import { Request, Response } from 'express';
-import Stripe from 'stripe';
-import dotenv from 'dotenv';
+import { Request, Response } from "express";
+import Stripe from "stripe";
+import dotenv from "dotenv";
 
 dotenv.config();
 
-// Usamos la clave secreta de Stripe, cargada directamente aquí
-const stripe = new Stripe('sk_test_51RkahlFL65NjHwNQbqZ2GU4bvnXCj7D0um9XdLDBNn53RES7H6eHzXQpkDNXD6kR0cwBJQA5sZ35wMgf8KKFgdB500yYqDmUm5', {
-  apiVersion: '2025-02-24.acacia',
+// Usamos la clave secreta de Stripe desde variable de entorno
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2025-02-24.acacia",
 });
 
 // Controlador para crear el Payment Intent
 const createPaymentIntent = async (req: Request, res: Response) => {
-  const { amount } = req.body;  // Monto a cobrar
+  const { amount } = req.body; // Monto a cobrar
 
   if (!amount) {
-    res.status(400).send({ error: 'Monto requerido' });
+    res.status(400).send({ error: "Monto requerido" });
     return;
   }
 
   try {
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
-      currency: 'usd',
-      description: 'Pago por Póliza',
+      currency: "usd",
+      description: "Pago por Póliza",
     });
 
     // Enviamos el client_secret al frontend para el proceso de pago
@@ -39,7 +39,11 @@ const confirmPayment = async (req: Request, res: Response) => {
   const { paymentMethodId, clientSecret } = req.body;
 
   if (!paymentMethodId || !clientSecret) {
-    res.status(400).json({ error: 'Faltan datos requeridos (paymentMethodId o clientSecret)' });
+    res
+      .status(400)
+      .json({
+        error: "Faltan datos requeridos (paymentMethodId o clientSecret)",
+      });
     return;
   }
 
@@ -49,16 +53,18 @@ const confirmPayment = async (req: Request, res: Response) => {
       payment_method: paymentMethodId,
     });
 
-    if (paymentIntent.status === 'succeeded') {
+    if (paymentIntent.status === "succeeded") {
       res.status(200).json({ success: true, paymentIntent });
       return;
     } else {
-      res.status(400).json({ error: 'El pago no fue procesado correctamente' });
+      res.status(400).json({ error: "El pago no fue procesado correctamente" });
       return;
     }
   } catch (error) {
-    console.error('Error al confirmar el pago:', error);
-    res.status(500).json({ error: (error as any).message || 'Error al confirmar el pago' });
+    console.error("Error al confirmar el pago:", error);
+    res
+      .status(500)
+      .json({ error: (error as any).message || "Error al confirmar el pago" });
     return;
   }
 };
